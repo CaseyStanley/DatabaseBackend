ce7a91f899dc34d28d18b4b9fc1dec7b3419fd95
CaseyStanley <stac3_16@uni.worc.ac.uk>
2018-02-05 11:38:43 +0000

    Update index.js

--- www/index.js
+++ www/index.js
@@ -4,5 +4,16 @@ $(document).on("pageshow","#todoPage", onPageShow);
 
 function onPageShow() 
 {
-     console.log("page shown");
+      console.log("page shown");
+}
+
+Backendless.Data.of("TASKS").find().then(processResults).catch(error);
+
+function processResults(tasks) 
+{
+ //display the first task in an array of tasks.
+     alert(tasks[0].Task)
+}
+function error(err) {
+     alert(err);
 }
